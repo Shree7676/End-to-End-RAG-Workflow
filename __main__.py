82c9bbff89095dfa8d7ca9ad5d32825157ba48a3
Shree7676 <shreekantnandiyawar@gmail.com>
2025-02-27 14:09:08 +0100

    solving deployment out of ram issue

--- __main__.py
+++ __main__.py
@@ -1,17 +1,17 @@
-import dotenv
-dotenv.load_dotenv()
+# import dotenv
+# dotenv.load_dotenv()
 
-from src import App
-import logging
-import logging.config
-import os
+# from src import App
+# import logging
+# import logging.config
+# import os
 
 
-log_file_path = os.path.join(os.path.dirname(__file__), 'logging.conf')
+# log_file_path = os.path.join(os.path.dirname(__file__), 'logging.conf')
 
-logging.config.fileConfig(log_file_path, disable_existing_loggers=False)
-logger = logging.getLogger()
+# logging.config.fileConfig(log_file_path, disable_existing_loggers=False)
+# logger = logging.getLogger()
 
-if __name__ == '__main__':
-  app = App()
-  app.run()
+# if __name__ == '__main__':
+#   app = App()
+#   app.run()
